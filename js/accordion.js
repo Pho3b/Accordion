12ed5ac724b6a528ad53c6583a668305736de727
Pho3b <andrea.rinaldi54@gmail.com>
2019-05-25 15:07:32 +0200

    Added javascript event to open and close the accordions

--- js/accordion.js
+++ js/accordion.js
@@ -1,38 +1,42 @@
 "use strict";
 
-function Accordion(options){
+class Accordion {
 	
 	//PROPERTIES
-	this.fragment = document.createDocumentFragment();						//Instantiating a document fragment
-	this.container = document.getElementById(options.container);
-	this.main_accordion = document.createElement('section');
-	this.main_accordion_title;
-	this.accordion_title;
-	this.accordion_subtitle;
-	this.accordion_block;
-	this.accordion_counter = 1;
-	this.accordion_icon;
-	this.content;
+	constructor(options) {
+		this.fragment = document.createDocumentFragment();						//Instantiating a document fragment
+		this.container = document.getElementById(options.container);
+		this.main_accordion = document.createElement('section');
+		this.main_accordion_title;
+		this.accordion_title;
+		this.accordion_subtitle;
+		this.accordion_block;
+		this.accordion_icon;
+		this.content;
+		
+		//Appending text of the main panel
+		this.main_accordion_title = document.createElement('p');
+		
+		this.main_accordion.appendChild(this.main_accordion_title);
+		this.main_accordion_title.innerHTML = options.mainTitle;
+		this.main_accordion_title.classList += 'main_text';
+		//Adding the ID for the main panel
+		this.main_accordion.setAttribute('id', 'panel_0');
+		this.main_accordion.className = 'main_accordion';	
+		//Appending the main panel to the container
+		this.container.appendChild(this.main_accordion);
+		
+		this.panels = options.panels;
+		
+		
+		//Initializer(Da mettere nel costruttore)
+		this.init();
+	}
+	
 
 	
-	//Appending text of the main panel
-	this.main_accordion_title = document.createElement('p');
-	
-	this.main_accordion.appendChild(this.main_accordion_title);
-	this.main_accordion_title.innerHTML = options.mainTitle;
-	this.main_accordion_title.classList += 'main_text';
-	//Adding the ID for the main panel
-	this.main_accordion.setAttribute('id', 'panel_0');
-	this.main_accordion.className = 'main_accordion';	
-	//Appending the main panel to the container
-	this.container.appendChild(this.main_accordion);
-	
-	this.panels = options.panels;
-	
-	
-	
 	//METHODS
-	this.createPanelsBlock = function (option) {
+	createPanelsBlock(option) {
 		
 		for (let option in this.panels) {
 			//Creating node elements for panels
@@ -55,10 +59,9 @@ function Accordion(options){
 			this.accordion_icon.className = 'material-icons md-26';
 			this.accordion_subtitle.className = 'accordion_subtitle';
 			this.content.className = 'content';
+			
+			
 				
-			
-			
-			
 			//Appending the created elements and building the actual panel block
 			this.accordion_block.appendChild(this.accordion_icon);
 			this.accordion_block.appendChild(this.accordion_title);
@@ -66,24 +69,39 @@ function Accordion(options){
 			this.accordion_block.appendChild(this.content);
 			//this.container.appendChild(this.accordion_block);
 			
+			//Adding Event Listener
+			this.accordion_block.addEventListener('click', this.open);
+			
 			//Appending to the fragment 
 			this.fragment.appendChild(this.accordion_block);
 			
-			//Incrementing the counter for the IDs
-			this.accordion_counter++;
 		}
 	};
 	
 	
-	this.init = function () {
+	open(){
+		let accordions = this.parentNode.childNodes;
+		let len = accordions.length;
+
 		
-		this.createPanelsBlock();
-		
-		this.container.appendChild(this.fragment);
+		for(let i = 0; i < len; i++) {	
+			if (accordions[i].classList.contains('open'))
+				accordions[i].classList.remove('open');
+		}
+		this.classList.add('open');
 	}
 	
 	
+	getAccordions() {
+		console.log(this.accordions);
+		return this.accordions;
+	}
 	
-	//Initializer(Da mettere nel costruttore)
-	this.init();
+	// Initializer
+	init() {
+		this.createPanelsBlock();
+		this.container.appendChild(this.fragment);
+	}
+	
+
 }
