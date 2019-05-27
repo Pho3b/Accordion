c52104dd3ec8183566663226dbe0cb30f21bec21
Pho3b <andrea.rinaldi54@gmail.com>
2019-05-27 15:52:41 +0200

    Cleaned up the project and commented the code

--- js/accordion.js
+++ js/accordion.js
@@ -2,21 +2,25 @@
 
 class Accordion {
 	
-	//PROPERTIES
+	//***************** PROPERTIES **********************//
+
 	constructor(options) {
-		//Instantiating a document fragment, it takes less resources than using innerHTML for the whole accordiong HTML structure
+		//Instantiating a document fragment, it takes less resources than using innerHTML to create the whole accordion structure
 		this.fragment = document.createDocumentFragment();
 		this.container = document.getElementById(options.container);
 		this.panels = options.panels;
-		this.accordion_counter = 0;	//Setting it to 1 to skip the main accordion
+		//Setting it to 1 in the init method to skip the Main Accordion block
+		this.accordion_counter = 0;
 
 		//Initializer
 		this.init();
 	}
 	
 
-	
-	//METHODS
+
+	//***************** METHODS **********************//
+
+	//IT GENERATES THE MAIN ACCORDION PANEL (The header) IT GETS CALLED ONLY IF THE MAIN TITLE IS SET BY THE USER
 	generateHeaderPanel() {
 		//Generating the header accordion (If title is left blank it won't appear)
 		this.main_accordion = document.createElement('section');
@@ -32,6 +36,7 @@ class Accordion {
 	}
 
 
+	//THIS METHOD ACTUALLY GENERATES THE HTML ACCORDION BLOCKS CYCLING TROUGH THE OPTIONS PANEL OBJECTS.
 	generatePanelsBlock() {
 
 		for (let option in this.panels) {
@@ -49,6 +54,7 @@ class Accordion {
 			this.right_column = document.createElement('div');
 			this.left_column.className = 'column left';
 			this.right_column.className = 'column right';
+			//Adding it to the main block
 			this.accordion_block.appendChild(this.left_column);
 			this.accordion_block.appendChild(this.right_column);
 
@@ -57,12 +63,15 @@ class Accordion {
 			this.accordion_title = document.createElement('p');
 			this.accordion_title.innerHTML = this.panels[option].title;
 			this.accordion_title.className = 'accordion_title';
+			//Adding it to the main block
 			this.left_column.appendChild(this.accordion_title);
 
 
 			//Creating the icon Element
 			this.accordion_icon = document.createElement('i');
 			this.accordion_icon.innerHTML = 'expand_more';
+			//Adding it to the main block
+			this.right_column.appendChild(this.accordion_icon);
 
 
 			//Generating the accordion Subtitle Paragraph (Only if not null or empty)
@@ -71,42 +80,33 @@ class Accordion {
 				this.accordion_subtitle.innerHTML = this.panels[option].subtitle;
 				this.accordion_subtitle.className = 'accordion_subtitle';
 				this.accordion_icon.className = 'material-icons md-26 with_subtitle';
+				//Adding it to the main block
 				this.left_column.appendChild(this.accordion_subtitle);
 			}else{
 				this.accordion_icon.className = 'material-icons md-26 no_subtitle';
 			}
 
 
+			//Generating the accordion Content element
 			this.content = document.createElement('div');
-
-			
-			//Setting the inner HTML for panels
 			this.content.innerHTML = this.panels[option].content;
-			
-			//Adding classes and IDs to the panels elements
-
 			this.content.className = 'content';
-
-
-			//Appending the created elements and building the actual panel block
-
-			this.right_column.appendChild(this.accordion_icon);
 			this.accordion_block.appendChild(this.content);
-			//this.container.appendChild(this.accordion_block);
 
-			//Adding Event Listener
+
+			//Adding the Event Listener
 			this.accordion_block.addEventListener('click', this.open);
 
-			//Appending to the fragment 
+			//Appending the current block to the fragment
 			this.fragment.appendChild(this.accordion_block);
 			
-			//Adding accordion counter
+			//Increasing accordion counter (I set this counter as an attribute of every block, it helps to indentify every single accordion block)
 			this.accordion_counter++
-			
 		}
 	};
 	
-	
+
+	//THE METHODS THAT GETS ATTACHED ON THE CLICK EVENT TO EVERY ACCORDION BLOCK, TOGGLES CLASS AND ICONS.
 	open(){
 		let accordions = this.parentNode.childNodes;
 		let len = accordions.length;
@@ -120,7 +120,6 @@ class Accordion {
 
 		//Toggling the class (open or not)
 		this.classList.toggle('open');
-		console.log(this);
 
 		//Toggling the icon
 		if(this.childNodes[1].childNodes[0].innerHTML === 'expand_less')
@@ -130,13 +129,13 @@ class Accordion {
 	}
 	
 
-	// Initializer
+	//MAIN METHOD WHICH INITIALIZE EVERY OTHER METHOD TO CREATE THE FINAL ACCORDION STRUCTURE
 	init() {
+		//Checking if the mainTitle property is set (If not i hide the mainTitle block)
 		if (options.mainTitle !== ''){
 			this.generateHeaderPanel();
 			this.accordion_counter = 1;
 		}
-
 
 		this.generatePanelsBlock();
 		this.container.appendChild(this.fragment);
